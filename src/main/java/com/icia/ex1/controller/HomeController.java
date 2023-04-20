ddfb54b12c9860b63c84de2b89f7607a93a291a2
swknjj <sinwook9255@gmail.com>
2023-04-20 17:08:56 +0900

    2023_04_20

--- src/main/java/com/icia/ex1/controller/HomeController.java
+++ src/main/java/com/icia/ex1/controller/HomeController.java
@@ -1,16 +1,49 @@
 package com.icia.ex1.controller;
 
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller // 스프링문법 어노테이션
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/") //  => / 주소로 get 요청이 오는경우 이 메서드 실행
     public String index() {
         return "index";
     }
 
+    // /home 요청 처리
+    // 메서드 이름: home, 주소요청방식: get
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 
+    // /wakeup 요청 처리
+    @GetMapping("/wakeup")
+    public String fun1() {
+        return "wakeup";
+    }
 
+    // req-param1 요청 처리
+    @GetMapping("/req-param1")
+    public String reqParam1(HttpServletRequest request) {
+        // email값 꺼내기
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        System.out.println("email = " + email);
+        System.out.println("password = " + password);
+        // 요청방식 조회
+        System.out.println("request.getMethod() = " + request.getMethod());
+        // 요청 url 조회
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        return "index";
+    }
+
+    @GetMapping("/req-param2")
+    public String reqParam2(@RequestParam("name") String name, @RequestParam("age") int age) {
+        System.out.println("name = " + name + ", age = " + age);
+        return "index";
+    }
 }
